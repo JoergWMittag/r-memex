ebdbb0ce784d3f0401504c8a60f141f136b4918f
Jörg W Mittag <JoergWMittag+R-Memex@GoogleMail.Com>
2008-07-12 00:57:56 +0000

    Create test suite for easy running of all tests.
    Create an integrated test suite that allows *all* tests to be run from
    one single file.

---
+++ test/r_memex_test_suite.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+require 'test_relation'
+require 'test_node'
+require 'test_node_container'
+require 'test_lastfm_generator'
+require 'test_interface_highline'
