cc71bf2f4d04fee830d866c6008a949adc0180a1
Jörg W Mittag <JoergWMittag+R-Memex@GoogleMail.Com>
2008-07-12 00:58:05 +0000

    Refactor the test suite to discover test files automagically.
    Discover the tests in the test suite automagically by just scanning the
    /test directory, instead of hardcoding all tests in the test suite.

--- test/r_memex_test_suite.rb
+++ test/r_memex_test_suite.rb
@@ -2,8 +2,6 @@
 
 require File.join(File.dirname(__FILE__), 'test_helper')
 
-require 'test_relation'
-require 'test_node'
-require 'test_node_container'
-require 'test_lastfm_generator'
-require 'test_interface_highline'
+Dir[File.join(File.dirname(__FILE__), '**/test_*.rb')].reject do |file|
+  file =~ /test_(helper|suite)\.rb$/
+end.each { |test| require test }
